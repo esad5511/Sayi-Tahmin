ff2f24d7ac44863a80284c37e3ebf93f37fa6f81
esad5511 <anescan5511@gmail.com>
2022-08-14 21:44:34 +0200

    ilk taahhüt

---
+++ main.js
@@ -0,0 +1,50 @@
+var sayac,ustsayi,altsayi,tahmin;
+
+var Basla=document.getElementById("Basla");
+var Yukari=document.getElementById("Yukari");
+var Asagi=document.getElementById("Asagi");
+var Bildi=document.getElementById("Bildi");
+var Durum=document.getElementById("Durum");
+
+Basla.onclick=function(){
+sayac=0;
+ustsayi=100;
+altsayi=0;
+tahmin=50;
+Durum.innerHTML="Bilgisayarın Tahmini:"+tahmin;
+Basla.disable=true;
+
+hesap();
+
+}
+
+Yukari.onclick=function(){
+    altsayi=tahmin;
+    tahmin=tahmin+((ustsayi-altsayi)/2);
+        tahmin=Math.floor(tahmin);
+        Durum.innerHTML="Bilgisayarın Tahmini:"+tahmin;
+    hesap();
+    
+    }
+ Asagi.onclick=function(){
+        ustsayi=tahmin;
+        tahmin=tahmin-((ustsayi-altsayi)/2);
+        tahmin=Math.floor(tahmin);
+        Durum.innerHTML="Bilgisayarın Tahmini:"+tahmin;
+        hesap();
+        
+    }
+ 
+
+Bildi.onclick=function(){
+    Durum.innerHTML="Bilgisayar "+sayac+" Tahminde Bildi!";
+     Basla.disabled = false;
+        
+ }
+ function hesap(){
+    sayac++;
+    
+
+    
+
+    }
